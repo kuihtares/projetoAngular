d719f51d253309e0dfb5552b1333069547f06134
Carlos Serathiuk <dudu@serathiuk.com>
2024-10-01 18:05:03 -0300

    Initial commit

---
+++ src/main/java/io/github/kuihtares/projetoAngular/ProjetoAngularAplication.java
@@ -0,0 +1,11 @@
+package io.github.kuihtares.projetoAngular;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAngularAplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/example/projetoangularerp/ProjetoAngularErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoangularerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAngularErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
